13b77d2bc36b0ad787ec27edbd73da3628b457e4
David Piper <david.piper@mailfence.com>
2019-11-06 10:33:13 -0600

    feat: complete js challenge
    Signed-off-by: David Piper <david.piper@mailfence.com>

--- frontend-interview-testfrom-eduardo-flores/src/script.js
+++ frontend-interview-testfrom-eduardo-flores/src/script.js
@@ -2,34 +2,40 @@
  * Note: jQuery is already included.
  */
 
-
 /********************************
  * QUESTION 1.
  *******************************/
 function executeQ1() {
-  // Answer goes here.
+  document.querySelector("#my-name").textContent = "David Piper";
 }
 
 /********************************
  * QUESTION 2.
  *******************************/
 var listItems = [
-  'Settings',
-  'Customer Support',
-  'On Demand',
-  'Search',
-  'Widgets'
+  "Settings",
+  "Customer Support",
+  "On Demand",
+  "Search",
+  "Widgets"
 ];
 
 function executeQ2() {
-  // Answer goes here.
+  const listItemNodes = listItems.map(value => {
+    const node = document.createElement("li");
+    node.appendChild(document.createTextNode(value + "\n"));
+    return node;
+  });
+  listItemNodes.forEach(value =>
+    document.querySelector("#q2-list").appendChild(value)
+  );
 }
 
 /********************************
  * QUESTION 3.
  *******************************/
 function Person() {
-  var name = '';
+  var name = "";
 
   this.setName = function(n) {
     name = n;
@@ -37,22 +43,46 @@ function Person() {
 
   this.getName = function() {
     return name;
-  }
+  };
 }
 
 function executeQ3() {
-  // Answer goes here.
+  const person1 = new Person();
+  person1.setName("Scott");
+  const person2 = new Person();
+  person2.setName("Matt");
+
+  const persons = [person1, person2];
+
+  const personNodes = persons.map(value => {
+    const node = document.createElement("li");
+    node.appendChild(document.createTextNode(value.getName() + "\n"));
+    return node;
+  });
+  personNodes.forEach(value =>
+    document.querySelector("#q3-list").appendChild(value)
+  );
 }
 
 /********************************
  * QUESTION 4.
  *******************************/
 function executeQ4() {
-  var fileURL = 'https://hydracdn.frontiertv.com/widgets/common/ch131images.json';
-  
+  var fileURL =
+    "https://hydracdn.frontiertv.com/widgets/common/ch131images.json";
+
   // Save image IDs in the data array.
   var data = [];
-  
-  // Get the data in fileURL using AJAX, process the reponse and add it to the HTML.
-}
 
+  // Get the data in fileURL using AJAX, process the reponse and add it to the HTML.
+
+  const response = fetch(fileURL).then(rawValue => {
+    rawValue.json().then(valueJson => {
+      const imageKeysArray = Object.keys(valueJson.images);
+      const answerNode = document.createTextNode(
+        JSON.stringify(imageKeysArray)
+      );
+      document.querySelector("#q4-answer").appendChild(answerNode);
+    });
+  });
+}
